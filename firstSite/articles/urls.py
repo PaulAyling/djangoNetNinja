0eb48a6f2ce53fba399a1f13c8f31da05e8642b4
PaulAyling <jimmyyukka@gmail.com>
2021-11-06 16:12:44 +0100

    finnished tutorial 11

--- firstSite/articles/urls.py
+++ firstSite/articles/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.article_list)
+    path('', views.article_list),
+    path('/', views.article_detail)
+
 ]
+# This has been significantly changed in Django 3, this could be done like this:path('/', views.article_detail),
+# where the first slug "" is the name of the parameter which could be any name; "abc", "the_slug", etc.
+# If you still insist on using regex then there is one thing to change; the method name instead of "url" 
+# use "re_path":re_path(r'^(?P[\w-]+)/$', views.article_detail),
